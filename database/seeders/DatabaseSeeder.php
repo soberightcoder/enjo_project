3b3baa62339bd27e6360374a4e81eb3ac0032715
qiushenwen <sobercoderq@gmail.com>
2026-05-27 16:45:31 +0800

    db:seed faker数据填充

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //在users表插入十条数据；
+        //单进程的数据的填充；
+        //User::factory()->count(10)->create();
     }
 }
